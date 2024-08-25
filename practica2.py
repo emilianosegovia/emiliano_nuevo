505fbe1248be53e20ef0e55e917389f0d8157a09
emilianosegovia <142274027+emilianosegovia@users.noreply.github.com>
2024-08-25 18:58:20 -0300

    Add files via upload

--- practica2.py
+++ practica2.py
@@ -4,14 +4,14 @@ nombre_archivo = 'cronograma_sugerido.csv'
 
 def materias_cuatrimestre(nombre_archivo,n):
     lista=[]
-    diccionario=dict()
     with open(nombre_archivo,mode='rt') as archivo:
         lector_csv=csv.reader(archivo,delimiter=';')
-        next(lector_csv)
+        primer_linea=next(lector_csv)
         for linea in lector_csv:
-            for i in range(len(linea)):
-                if int(linea[0])==(n):
-                    diccionario[lector_csv[0][i]]=linea[i]
+            if int(linea[0])==(n):
+                diccionario=dict()
+                for i in range(len(primer_linea)):
+                    diccionario[primer_linea[i]]=linea[i]
                 lista.append(diccionario)
     return lista
 print(materias_cuatrimestre(nombre_archivo, 3))
